513fe92fea152ed9389e8e015003090ace55d2d2
zhimin zhou <zzmax.zhou@gmail.com>
2014-10-01 15:08:15 +0200

    zhimin

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -6,15 +6,19 @@ import javax.swing.JPanel;
 
 public class MyDrawing extends JPanel {
 	private Cat cat;
+	private Cat cat2;
 	public MyDrawing() 
 	{
 		// Add critters to the drawing
 		cat = new Cat();
+		cat2 = new Cat();
+		
 	}
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
 		cat.draw(g, 50, 50);
+		cat.draw(g, 150, 250);
 	}
 	
 	/**
